3362256aad38bd5815dd363ed9b4e932bdb3f1f0
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-09-28 14:52:29 +0100

    backend

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './Login.css';
-import { Link } from 'react-router-dom';
-import pic from '../pic.svg';
+import React, { useState } from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+import pic from "../pic.svg";
 
 function Login() {
   return (
