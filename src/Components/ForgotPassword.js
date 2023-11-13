6bb963708067025eb118812c4002bab53d2ff02a
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2023-11-13 08:06:18 +0100

    frgot pasword

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import "./ForgotPassword.css";
 import { Link } from "react-router-dom";
-
+import axiosInstance from "./axios/axios";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setEmail("");
+    try {
+      await axiosInstance.post("/forgotPassword", email).then((response) => {
+        setStatus(response.data.Status);
+        console.log(response);
+        // if (!response.data.error) {
+        //   setStatus("An Email has been sent to your registered email address!");
+        // } else {
+        //   setStatus("Invalid Username or Password");
+        // }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="reset-body">
       <div className="sub-body">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="title text-xl font-semibold">Forgot Password</div>
           {/* <div className="message">
               <p>Enter your email and we'll send you a link to reset your password</p>
@@ -17,16 +38,19 @@ const ForgotPassword = () => {
               Enter your email and we'll send you a recovery code to reset your
               password
             </p>
-            <input type="email" placeholder="Enter email" />
+            <input
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
-          <div className="button-container">
-            <button className="btn btn-primary">
-              <Link to="/ResetCode">Submit</Link>
-            </button>
-          </div>
+          <span>{status}</span>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
           <div className="login-reroute">
             <Link to="/">Back to Login</Link>
-
           </div>
         </form>
       </div>
